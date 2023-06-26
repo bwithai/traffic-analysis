222708a7fd1a746247e95ce9af99c9e007ea996f
Sanaullah <sanaullah41821@gmail.com>
2023-06-26 18:30:30 +0500

    add comments

--- main.py
+++ main.py
@@ -23,6 +23,15 @@ app.add_middleware(
 
 @app.post("/api/v1/upload")
 async def create_upload_file(file: UploadFile = File(...)):
+    """
+       Uploads a file to the server.
+
+       Args:
+           file (UploadFile): The file to be uploaded.
+
+       Returns:
+           dict: Message indicating if the file was uploaded successfully.
+    """
     if not file.read():
         return {"message": "No upload file sent"}
 
@@ -39,6 +48,15 @@ async def create_upload_file(file: UploadFile = File(...)):
 
 
 def get_file_names(folder_path):
+    """
+        Retrieves the names of all files in a folder.
+
+        Args:
+            folder_path (str): Path to the folder.
+
+        Returns:
+            list: List of file names in the folder.
+    """
     file_names = []
     for filename in os.listdir(folder_path):
         if os.path.isfile(os.path.join(folder_path, filename)):
@@ -48,6 +66,12 @@ def get_file_names(folder_path):
 
 @app.get("/api/v1/video/get-all-file-name")
 async def get_all_stored_file():
+    """
+        Retrieves the names of all demo files and detected videos.
+
+        Returns:
+            dict: Dictionary containing the lists of demo files and detected videos.
+    """
     demo = []
     result = []
     demo_files = get_file_names("demo")
@@ -69,11 +93,18 @@ async def get_all_stored_file():
 
 @app.post("/api/v1/load-traffic-analysis-system")
 async def load_traffic_analysis_system(metadata: DetectResponseModel):
-    # detected_video_path = load_system(source=file_path, draw_paths=metadatadraw_paths,
-    #                                   classes=metadataclasses, track_boxes=metadatatrack_boxes,
-    #                                   save=metadatasave)
-    detected_video_path= load_system(source="./demo/traffic.mp4", draw_paths=metadata.draw_paths, classes=[2, 3],
-                id_size=metadata.id_size, path_history=metadata.path_history, draw_objects=metadata.draw_objects,
-                track_boxes=metadata.track_boxes, save=metadata.save, mask_detections=True)
+    """
+        Loads the traffic analysis system and processes the video.
+
+        Args:
+            metadata (DetectResponseModel): Metadata for processing the video.
+
+        Returns:
+            FileResponse: Response containing the processed video file.
+    """
+    detected_video_path = load_system(source="./demo/traffic.mp4", draw_paths=metadata.draw_paths, classes=[2, 3],
+                                      id_size=metadata.id_size, path_history=metadata.path_history,
+                                      draw_objects=metadata.draw_objects,
+                                      track_boxes=metadata.track_boxes, save=metadata.save, mask_detections=True)
 
     return FileResponse(detected_video_path, media_type="video/mp4", filename="traffic_out.mp4")
